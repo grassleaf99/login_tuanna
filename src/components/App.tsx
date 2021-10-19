04bf2746b3499c3435a7732a888be6f84b9553e4
grassleaf99 <tuanna.B17CN654@stu.ptit.edu.vn>
2021-10-19 17:02:33 +0700

    use Provider, createStore, useSelector, useDispatch to track changes in input fields

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,17 +6,30 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import loginReducer from "../store/reducers";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./Login";
+
+const store = createStore(
+  loginReducer,
+  typeof window === "object" &&
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : (f) => f
+);
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact>
-          <Redirect to="/login"></Redirect>
-        </Route>
-        <Route path="/login" exact component={Login} />
+        <Provider store={store}>
+          <Route path="/" exact>
+            <Redirect to="/login"></Redirect>
+          </Route>
+          <Route path="/login" exact component={Login} />
+        </Provider>
       </Switch>
     </Router>
   );
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,16 +1,35 @@
 import React, { FunctionComponent } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Form, Button } from "react-bootstrap";
+import {
+  changeUsernameField,
+  changePasswordField,
+} from "../store/action_creators";
+import State from "../store/state";
 
 const Login: FunctionComponent = () => {
+  const authenData = useSelector((state: State) => state);
+  const dispatch = useDispatch();
+
   return (
     <section className="sec-login">
       <div className="login-form p-5">
         <Form>
           <Form.Group className="mb-4" controlId="formBasicEmail">
-            <Form.Control type="text" placeholder="Username" />
+            <Form.Control
+              type="text"
+              placeholder="Username"
+              value={authenData.username}
+              onChange={(e) => dispatch(changeUsernameField(e.target.value))}
+            />
           </Form.Group>
           <Form.Group className="mb-4" controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              value={authenData.password}
+              onChange={(e) => dispatch(changePasswordField(e.target.value))}
+            />
           </Form.Group>
           <Button
             variant="success"
