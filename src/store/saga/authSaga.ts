5bb476dd0dc8f38db7b8f7f4b227b17a81699c26
grassleaf99 <tuanna.B17CN654@stu.ptit.edu.vn>
2021-10-24 08:46:14 +0700

    fix bug: yield call (api return Promise) return AxiosResponse<type of data of AxiosResponse>

--- src/store/saga/authSaga.ts
+++ src/store/saga/authSaga.ts
@@ -2,6 +2,30 @@ import { takeEvery, takeLatest, call } from "redux-saga/effects";
 import ActionTypes from "../types";
 import axios, { AxiosResponse } from "axios";
 
+interface HttpbinResponse {
+  args: {};
+  data: string;
+  files: {};
+  form: {};
+  headers: {
+    "Accept-Encoding": string;
+    "Content-Length": number;
+    "Content-Type": string;
+    Host: string;
+    "User-Agent": string;
+    "X-Amzn-Trace-Id": string;
+  };
+  json: null;
+  origin: string;
+  url: string;
+}
+
+function* authSaga() {
+  yield takeEvery("CHANGE_USERNAME_FIELD" as ActionTypes, showLogUsername);
+  yield takeEvery("CHANGE_PASSWORD_FIELD" as ActionTypes, showLogPassword);
+  yield takeLatest("LOGIN_REQUEST" as ActionTypes, loginRequestSaga);
+}
+
 function showLogUsername() {
   console.log("Username has changed!!");
 }
@@ -9,7 +33,10 @@ function showLogPassword() {
   console.log("Password has changed!!");
 }
 
-function apiLogin(loginData: { username: string; password: string }) {
+function apiLogin(loginData: {
+  username: string;
+  password: string;
+}): Promise<AxiosResponse<HttpbinResponse>> {
   return axios.post("https://httpbin.org/post", {
     username: loginData.username,
     password: loginData.password,
@@ -21,18 +48,12 @@ function* loginRequestSaga(action: {
   payload: { username: string; password: string };
 }) {
   try {
-    const res: Promise<AxiosResponse> = yield call(apiLogin, {
+    const res: AxiosResponse<HttpbinResponse> = yield call(apiLogin, {
       username: action.payload.username,
       password: action.payload.password,
     });
-    console.log(res);
+    console.log(res.data.data);
   } catch (error) {}
-}
-
-function* authSaga() {
-  yield takeEvery("CHANGE_USERNAME_FIELD" as ActionTypes, showLogUsername);
-  yield takeEvery("CHANGE_PASSWORD_FIELD" as ActionTypes, showLogPassword);
-  yield takeLatest("LOGIN_REQUEST" as ActionTypes, loginRequestSaga);
 }
 
 export default authSaga;
